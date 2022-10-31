20adf2cac200416c1914e22b5c2ce8202e56ff28
erramifadelellah && ayman eljamaaoui <fadelellaherrami@gmail.com>
2022-10-31 19:50:48 +0100

    callBack

--- asynch.js
+++ asynch.js
@@ -27,18 +27,18 @@
     errami fadlolah
 
 */
-console.log("errami1");
-console.log("errami2");
-let user = getUser(7);
-console.log(user);
+// console.log("errami1");
+// console.log("errami2");
+// let user = getUser(7);
+// console.log(user);
 
-console.log("errami3");
-function getUser(id){
-    setTimeout(()=>{
-        console.log("Bonjour monsieur ERRAMI Fadel Ellah");
-        return {id: id,name: "Fadel Ellah ERRAMI",}
-    });
-}
+// console.log("errami3");
+// function getUser(id){
+//     setTimeout(()=>{
+//         console.log("Bonjour monsieur ERRAMI Fadel Ellah");
+//         return {id: id,name: "Fadel Ellah ERRAMI",}
+//     });
+// }
 /*
     errami1
     errami2
@@ -47,3 +47,74 @@ function getUser(id){
     Bonjour monsieur ERRAMI Fadel Ellah
 
 */
+
+
+// callBack :
+/*
+    Une fonction de rappel (aussi appelée callback en anglais) est une fonction passée dans une autre 
+    fonction en tant qu'argument, qui est ensuite invoquée à l'intérieur de la fonction externe pour 
+    accomplir une sorte de routine ou d'action
+
+*/
+// console.log("errami1");
+// console.log("errami2");
+// let user = getUser(7,user=>{
+//     console.log(user);
+// });
+
+
+// console.log("errami3");
+// function getUser(id,callBack){
+//     setTimeout(()=>{
+//         console.log("Bonjour monsieur ERRAMI Fadel Ellah");
+//         return callBack({id: id,name: "Fadel Ellah ERRAMI",})//la fonction callBack s'écecute aprés l'éxecution de la fonction getUser()
+//     });
+// }
+
+/*
+==>
+    errami1
+    errami2
+    undefined
+    errami3
+    Bonjour monsieur ERRAMI Fadel Ellah
+    {id: 7, name: 'Fadel Ellah ERRAMI'}
+
+*/
+
+console.log("errami1");
+console.log("errami2");
+let user = getUser(7,user=>{
+    console.log(user);
+    getProduct(user.id,products=>console.log("products"+products));
+});
+
+
+console.log("errami3");
+function getUser(id,callBack){
+    setTimeout(()=>{
+        console.log("Bonjour monsieur ERRAMI Fadel Ellah");
+        return callBack({id: id,name: "Fadel Ellah ERRAMI",})//la fonction callBack s'écecute aprés l'éxecution de la fonction getUser()
+    });
+}
+function getProduct(userId,callBack){
+    setTimeout(()=>{
+        console.log("Bonjour Errami .......");
+        return callBack(['p1','p2']);
+    },2000);
+}
+
+/*
+==>
+    errami1
+    errami2
+    undefined
+    errami3
+    Bonjour monsieur ERRAMI Fadel Ellah
+    {id: 7, name: 'Fadel Ellah ERRAMI'}
+    Bonjour Errami .......
+    productsp1,p2
+
+    callBack n'est pas une bonne pratique car si on a beucoup des fonctions callBack ,on va avoir un code trés complexe
+*/
+
