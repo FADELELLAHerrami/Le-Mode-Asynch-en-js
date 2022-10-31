3a92944bf2f6c271ed31c155741b8d8c65e8d114
erramifadelellah && ayman eljamaaoui <fadelellaherrami@gmail.com>
2022-10-31 18:28:13 +0100

    ecrire en mode asynch

--- asynch.js
+++ asynch.js
@@ -4,9 +4,9 @@
 
 
 
- console.log("errami1");
- console.log("errami2");
- console.log("errami3");
+//  console.log("errami1");
+//  console.log("errami2");
+//  console.log("errami3");
  /*
     ==>
     errami1
@@ -15,15 +15,35 @@
  
  */
 
-    console.log("errami1");
-    setTimeout(()=>console.log("errami fadlolah"),4000)
-    console.log("errami2");
-    console.log("errami3");
+    // console.log("errami1");
+    // setTimeout(()=>console.log("errami fadlolah"),4000)
+    // console.log("errami2");
+    // console.log("errami3");
 /*
     ==>
     errami1
     errami2
     errami3
     errami fadlolah
+
+*/
+console.log("errami1");
+console.log("errami2");
+let user = getUser(7);
+console.log(user);
+
+console.log("errami3");
+function getUser(id){
+    setTimeout(()=>{
+        console.log("Bonjour monsieur ERRAMI Fadel Ellah");
+        return {id: id,name: "Fadel Ellah ERRAMI",}
+    });
+}
+/*
+    errami1
+    errami2
+    undefined
+    errami3
+    Bonjour monsieur ERRAMI Fadel Ellah
 
 */
